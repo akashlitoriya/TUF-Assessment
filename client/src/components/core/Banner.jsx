d5f350697dd9b56594b0f7539a9f0cd71382ffe0
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-08-13 01:32:40 +0530

    Fixed the banner cloase issue

--- client/src/components/core/Banner.jsx
+++ client/src/components/core/Banner.jsx
@@ -10,17 +10,19 @@ const Banner = () => {
   const {bannerTimer, bannerDescription, bannerLink} = useSelector(state => state.banner);
   console.log("BANNER TIMER : ", bannerTimer);  
   return (
-    <a href={bannerLink} target='_blank' className='max-w-screen-lg w-full mx-auto bg-richBlack-50 border-[1px] border-richBlack-25 p-10 rounded-lg relative cursor-pointer'>
-      <div className='absolute top-2 right-2 cursor-pointer' onClick={()=>dispatch(closeBanner())}>
+    <div className='max-w-screen-lg w-full mx-auto bg-richBlack-50 border-[1px] border-richBlack-25 p-10 rounded-lg relative'>
+      <div className='absolute top-2 right-2 cursor-pointer z-50' onClick={()=>dispatch(closeBanner())}>
         <RxCross2 className='text-richRed text-3xl'/>
       </div>
-      <div>
-        <p className='text-richRed text-2xl font-semibold'>{bannerDescription}</p>
-      </div>
-      <div>
-        <Timer bannerTimer={bannerTimer}/>
-      </div>
-    </a>
+      <a href={bannerLink} target='_blank' className='cursor-pointer'>
+        <div>
+          <p className='text-richRed text-2xl font-semibold'>{bannerDescription}</p>
+        </div>
+        <div>
+          <Timer bannerTimer={bannerTimer}/>
+        </div>
+      </a>
+    </div>
   )
 }
 
